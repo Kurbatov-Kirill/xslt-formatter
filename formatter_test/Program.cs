b7dd36cbc93060ab0c5061fe775256113889acb0
Kirill Kurbatov <iil.kurbatov.kirill@gmail.com>
2025-11-04 14:45:58 +0300

    Third commit
    Изменения:
    - Программа дописывает атрибут overall (сумма всех выплат по всем сотрудникам) в элемент Pay исходного файла XML

--- formatter_test/Program.cs
+++ formatter_test/Program.cs
@@ -7,8 +7,10 @@ public class XsltTransformation
 {
     public static void Main(string[] args)
     {
+        double employeeAmount = 0;
+        string employeeAmountStr = "0";
         double overallAmount = 0;
-        string overallAmountStr = "";
+        string overallAmountStr = "0";
         string xmlFilePath = "Data1.xml";
         string xsltFilePath = "formatter.xslt";
         string outputFilePath = "Employees.xml";
@@ -28,28 +30,44 @@ public class XsltTransformation
         {
             foreach (XmlElement xnode in xRoot)
             {
-                overallAmount = 0;
-                overallAmountStr = "0";
+                employeeAmount = 0;
+                employeeAmountStr = "0";
 
                 foreach (XmlNode childnode in xnode.ChildNodes)
                 {
                     XmlNode? amount = childnode.Attributes.GetNamedItem("amount");
                     string tempStr = amount.InnerText;
                     tempStr = tempStr.Replace(",", ".");
-                    Console.WriteLine(overallAmount + " + " + double.Parse(tempStr, CultureInfo.InvariantCulture) + " = " + (overallAmount + double.Parse(tempStr, CultureInfo.InvariantCulture)));
-                    overallAmount += double.Parse(tempStr, CultureInfo.InvariantCulture);
+                    Console.WriteLine(employeeAmount + " + " + double.Parse(tempStr, CultureInfo.InvariantCulture) + " = " + (employeeAmount + double.Parse(tempStr, CultureInfo.InvariantCulture)));
+                    employeeAmount += double.Parse(tempStr, CultureInfo.InvariantCulture);
                 }
                 Console.WriteLine();
 
-                overallAmountStr = overallAmount.ToString();
+                overallAmount += employeeAmount;
+                Console.WriteLine("Overall: " + overallAmount + " + " + employeeAmount + " = " + (overallAmount + employeeAmount));
+                employeeAmountStr = employeeAmount.ToString();
                 XmlAttribute sum = xDoc.CreateAttribute("sum");
-                XmlText amountVal = xDoc.CreateTextNode(overallAmountStr);
+                XmlText amountVal = xDoc.CreateTextNode(employeeAmountStr);
                 sum.AppendChild(amountVal);
                 XmlNode? attr = xnode.Attributes.Append(sum);
             }
         }
-        
+
         xDoc.Save("Employees.xml");
+        xDoc.Load("Data1.xml");
+
+        XmlElement? xRootS = xDoc.DocumentElement;
+
+        if (xRootS != null)
+        {
+            overallAmountStr = overallAmount.ToString();
+            XmlAttribute overall = xDoc.CreateAttribute("overall");
+            XmlText overalAmountVal = xDoc.CreateTextNode(overallAmountStr);
+            overall.AppendChild(overalAmountVal);
+            XmlNode? attr = xRootS.Attributes.Append(overall);
+        }
+
+        xDoc.Save("Data1.xml");
 
         Console.WriteLine($"Преобразование завершено. Результат сохранен в {outputFilePath}");
     }
