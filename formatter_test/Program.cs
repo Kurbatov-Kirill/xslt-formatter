15f3a2d126e2e57dffe898cbad7f379b6f84e42b
Kirill Kurbatov <iil.kurbatov.kirill@gmail.com>
2025-11-04 14:17:30 +0300

    Second commit
    Изменения:
    - После xslt-преобразования в выходной файл Employees.xml для каждого сотрудника (Employee) дописывается атрибут sum, в который записывается сумма всех выплат по этому сотруднику

--- formatter_test/Program.cs
+++ formatter_test/Program.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Xml;
 using System.Xml.Xsl;
+using System.Globalization;
 
 public class XsltTransformation
 {
     public static void Main(string[] args)
     {
+        double overallAmount = 0;
+        string overallAmountStr = "";
         string xmlFilePath = "Data1.xml";
         string xsltFilePath = "formatter.xslt";
         string outputFilePath = "Employees.xml";
@@ -17,6 +20,36 @@ public class XsltTransformation
         {
             transform.Transform(xmlFilePath, writer);
         }
+
+        XmlDocument xDoc = new XmlDocument();
+        xDoc.Load("Employees.xml");
+        XmlElement? xRoot = xDoc.DocumentElement;
+        if (xRoot != null)
+        {
+            foreach (XmlElement xnode in xRoot)
+            {
+                overallAmount = 0;
+                overallAmountStr = "0";
+
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    XmlNode? amount = childnode.Attributes.GetNamedItem("amount");
+                    string tempStr = amount.InnerText;
+                    tempStr = tempStr.Replace(",", ".");
+                    Console.WriteLine(overallAmount + " + " + double.Parse(tempStr, CultureInfo.InvariantCulture) + " = " + (overallAmount + double.Parse(tempStr, CultureInfo.InvariantCulture)));
+                    overallAmount += double.Parse(tempStr, CultureInfo.InvariantCulture);
+                }
+                Console.WriteLine();
+
+                overallAmountStr = overallAmount.ToString();
+                XmlAttribute sum = xDoc.CreateAttribute("sum");
+                XmlText amountVal = xDoc.CreateTextNode(overallAmountStr);
+                sum.AppendChild(amountVal);
+                XmlNode? attr = xnode.Attributes.Append(sum);
+            }
+        }
+        
+        xDoc.Save("Employees.xml");
 
         Console.WriteLine($"Преобразование завершено. Результат сохранен в {outputFilePath}");
     }
